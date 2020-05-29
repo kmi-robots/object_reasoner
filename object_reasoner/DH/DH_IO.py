dfb129e2db1ee9320afca3b3a65c0e809743ff4e
achiatti <agnese.chiatti@gmail.com>
2020-05-29 15:52:24 +0100

    Fixed DH interact

--- object_reasoner/DH/DH_IO.py
+++ object_reasoner/DH/DH_IO.py
@@ -1,43 +1,139 @@
 """
+In Python2 because it interfaces with ROS
+
 Methods to interact with Data Hub API:
-Requires key to access DH stored in .ini config file
+Requires key to access DH stored in a local .py file
 """
-import configparser
+import argparse
 import requests
 from requests.auth import HTTPBasicAuth
 import json
 import datetime
 import os
 import time
+import sys
+import rosbag
+from cv_bridge import CvBridge #, CvBridgeError
+import cv2
+from PIL import Image
+import numpy as np
+import base64
+from io import BytesIO
+import matplotlib.pyplot as plt
 
-config = configparser.ConfigParser()
-config.read('DHauth.ini')
-DHdict = config['DH_ACCESS']
-complete_url = os.path.join(str(DHdict['url']),'sciroc-episode12-image')
+try:
+    import DHauth as dh
+except:
+    print("Please create a local file named DHauth.py containing your Data Hub credentials")
+    sys.exit(0)
 
 def get_imgs():
-    print(type(DHdict['url']))
+    # print(type(DHdict['url']))
     t = datetime.datetime.fromtimestamp(time.time())
     timestamp = t.strftime("%Y-%m-%dT%H:%M:%SZ")
 
     json_body= {
         "@id": "string",
         "@type": "ImageReport",
-        "team": DHdict['teamid'],
+        "team": dh.teamid,
         "timestamp": timestamp,
         }
 
-    return requests.request("GET", complete_url, data=json.dumps(json_body),
-                            auth=HTTPBasicAuth(DHdict['teamkey'], ''))
+    return requests.request("GET", dh.url, data=json.dumps(json_body),
+                            auth=HTTPBasicAuth(dh.teamkey, ''))
+
+def post_img(input, tstamp, fbase, rgb=True):
+
+    if rgb:
+        _, buffer = cv2.imencode('.jpg', input)
+
+    else: #depth img
+        buffer = open(input, 'rb').read()
+
+    bstring = base64.b64encode(buffer).decode('utf-8')  # .encode('utf-8')
+
+    """
+    dstring = base64.b64decode(bstring)  # .decode('utf-8'))
+    dimg = Image.open(BytesIO(dstring))
+    darray = np.array(dimg)
+    plt.imshow(darray, cmap='Greys_r')
+    plt.show()
+    # print(darray.dtype)
+    """
+    complete_url = os.path.join(dh.url, fbase)
+    json_body ={"@id": fbase,
+                 "@type": dh.nodetype,
+                 "team": dh.teamid,
+                 "timestamp": tstamp,
+                 "x": 0,
+                 "y": 0,
+                 "z": 0,
+                 "base64": bstring,
+                 "format": "image/jpeg"
+                }
+    return requests.request("POST", complete_url, data=json.dumps(json_body),
+                            auth=HTTPBasicAuth(dh.teamkey, ''))
+
+def main():
+    """
+    Currently plays a local rosbag to export either RGB or depth data
+    as JPG image
+    + Sends the image to DH after converting to base64
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path_in', help="Path to input bag")
+    parser.add_argument('pat'
+                        'h_out', help="Where to store images locally")
+    args = parser.parse_args()
+
+    print("Loading rosbag from %s" % args.path_in)
+    bag = rosbag.Bag(args.path_in)
+    bridge = CvBridge()
+
+    #create 1 dir for rgb and 1 for depth imgs
+    if not os.path.isdir(os.path.join(args.path_out,'rgb')):
+        os.makedirs(os.path.join(args.path_out,'rgb'), exist_ok=True)
+    if not os.path.isdir(os.path.join(args.path_out,'depth')):
+        os.makedirs(os.path.join(args.path_out,'depth'), exist_ok=True)
+
+    print("Iterating through messages")
+    for topic, msg, t in bag.read_messages():
+        try:
+            # tstamp in header more realistic than t, i.e., time img is stored in the bag
+            timestamp = msg.header.stamp.to_sec()
+            t = datetime.datetime.fromtimestamp(timestamp)
+            timestamp = t.strftime("%Y-%m-%dT%H:%M:%SZ")
+            fname = str(t).replace('.','_').replace(' ','-').replace(':','-')
+        except AttributeError: continue
+
+        if topic =='/camera/rgb/image_raw':
+
+            rgb_img = bridge.imgmsg_to_cv2(msg, "passthrough")
+            filename = fname + ".jpg"
+            if not os.path.isfile(os.path.join(args.path_out,'rgb', filename)):
+                cv2.imwrite(os.path.join(args.path_out,'rgb', filename), rgb_img)
+                #send img to DH
+                res = post_img(rgb_img, timestamp, fname)
+                print(res.content)
+
+        elif topic=='/camera/depth/image_raw':
+
+            depth_data = bridge.imgmsg_to_cv2(msg, "32FC1")
+            # print(depth_data.dtype)
+            filename = fname + "-depth.tiff"
+            dimg = Image.fromarray(depth_data)
+            if not os.path.isfile(os.path.join(args.path_out, 'depth', filename)):
+                dimg.save(os.path.join(args.path_out, 'depth', filename))
+                #send to DH
+                res= post_img(os.path.join(args.path_out, 'depth', filename),timestamp, fname, rgb=False)
+                print(res.content)
+
+        else: continue
 
 
-def get_local():
-
-    with open(str(DHdict['local'])) as file:
-        j = json.load(file)
-        print(j[-1])
-    return j
+    print("RGB and depth images saved under %s" % args.path_out)
+    return 0
 
 if __name__ == "__main__":
 
-    print(get_local())
+    sys.exit(main())
